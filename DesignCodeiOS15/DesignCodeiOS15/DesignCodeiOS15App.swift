1b8af6bfbe421b742098643ba050daaaaa2591ea
Iñaki Narciso <ionicsoft@gmail.com>
2022-05-26 19:17:25 +0800

    Initial commit

---
+++ DesignCodeiOS15/DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by インヤキ on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
