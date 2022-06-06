23bec90caf02ced3e73b028ad6d2b42ad5759984
Iñaki Narciso <ionicsoft@gmail.com>
2022-06-06 16:30:57 +0800

    Lesson 4: Material and Foreground Style

--- DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
@@ -19,29 +19,51 @@ struct ContentView: View {
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundStyle(
+                    .linearGradient(
+                        colors: [.primary, .primary.opacity(0.5)],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
             Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             Text(/*@START_MENU_TOKEN@*/"Build an iOS app for iOS 15 with custom layouts, animations and ..."/*@END_MENU_TOKEN@*/)
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
         }
         .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(Color("Background"))
+        .background(.ultraThinMaterial)
         .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .padding(.horizontal, 20)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
