449a2d5ec95f4aae41e011c53130528a40cdbeb8
Iñaki Narciso <ionicsoft@gmail.com>
2022-06-09 22:14:49 +0800

    Lesson 8: SF Symbols 3

--- DesignCodeiOS15/DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/DesignCodeiOS15/AccountView.swift
@@ -11,12 +11,42 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                VStack(spacing: 8) {
+                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .symbolRenderingMode(.palette)
+                        .font(.system(size: 32))
+                        .foregroundStyle(.blue.opacity(0.3), .blue)
+                        .padding()
+                        .background(.ultraThinMaterial, in: Circle())
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundStyle(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                    )
+                    Text("@username")
+                        .font(.title)
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.large)
+                        Text("PH")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                
                 Section {
-                    Text("Settings")
+                    Label("Settings", systemImage: "gear")
+                        .imageScale(.large)
+                    Label("Billing", systemImage: "creditcard")
+                        .imageScale(.large)
+                    Label("Help", systemImage: "questionmark")
+                        .imageScale(.large)
                 }
                 .listRowSeparator(.hidden)
-                Text("Billing")
-                Text("Help")
             }
             .listStyle(.automatic)
             .navigationTitle("Account")
